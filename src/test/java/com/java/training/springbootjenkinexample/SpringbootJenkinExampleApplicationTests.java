ef7dfb8b626a9b490a17235d45f1f16af936505d
javaworldprogram <javaworldprogram@gmail.com>
2024-11-17 17:34:52 -0600

    Third Commit

--- src/test/java/com/java/training/springbootjenkinexample/SpringbootJenkinExampleApplicationTests.java
+++ src/test/java/com/java/training/springbootjenkinexample/SpringbootJenkinExampleApplicationTests.java
@@ -1,26 +1,22 @@
 package com.java.training.springbootjenkinexample;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertEquals;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootJenkinExampleApplicationTests {
 
     public static Logger logger = LoggerFactory.getLogger(SpringbootJenkinExampleApplicationTests.class);
 
     @Test
-    void contextLoads() {
-        logger.info("contextLoads Second Test Commit");
-        assertEquals(true, true);
+    public void contextLoads() {
+        logger.info("*********contextLoads Second Test Commit");
+
     }
 
 }
